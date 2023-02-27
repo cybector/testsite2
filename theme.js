1a0d5d501dd0a5f559ba7e195dc89c6adcfd6bb5
cybector <tylersphotography2018@gmail.com>
2023-02-27 05:07:48 -0500

    dd

--- theme.js
+++ theme.js
@@ -1,15 +1,12 @@
 // Provides background image and greeting based on time of day
 function changeBackground() {
-    var date = new Date();
-    var current_hour = date.getHours();
-    if (current_hour > 6 && current_hour < 18) {
-        document.getElementById("time").innerHTML = "Good morning";
-        document.body.style.backgroundImage = "url('images/best day.svg')";
-    } else {
-        var element = document.body;
-        element.classList.add("dark-mode");
+    var element = document.body;
+    if (element.classList.contains("dark-mode")) {
         document.getElementById("time").innerHTML = "Good Night";
         element.style.backgroundImage = "url('images/best night.svg')";
+    } else {
+        document.getElementById("time").innerHTML = "Good morning";
+        element.style.backgroundImage = "url('images/best day.svg')";
     }
 }
 
@@ -28,11 +25,13 @@ function changeBackgroundToggle() {
         document.getElementById("time").innerHTML = "Good morning";
         element.style.backgroundImage = "url('images/best day.svg')";
     }
+    // Update the background image when the sun is clicked
+    changeBackground();
+    $('.sun-checkbox').toggleClass('sun-checked');
 }
 
 $(document).ready(function () {
     $('.sun').click(function () {
-        // $(this).next('ul').slideToggle('500');
         $(this).find('i').toggleClass('fa-solid fa-sun fa-solid fa-moon');
         changeBackgroundToggle();
     });
